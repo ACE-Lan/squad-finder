cc54cea0dcdbcc78f8e1b4bdb8028b7932d31129
Ivan Nalon <ivannalon@gmail.com>
2023-02-19 23:54:40 -0300

    add document component and styles folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return (
+        <h1>Logo</h1>
+    )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Main, NextScript } from "next/document";
+import Head from "next/head";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default function Document(){
+    return(
+        <html>
+            <Head>
+                {/* TO DO - import font */}
+            </Head>
+
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </html>
+    )
 }
