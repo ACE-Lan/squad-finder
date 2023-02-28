8f78804e1c68bc34deb75adb7ba48696baaeb4cf
Ivan Nalon <ivannalon@gmail.com>
2023-02-28 16:35:33 -0300

    init firebase service

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,10 +2,6 @@ import { styled } from '@/styles/styles'
 
 // <Link prefetch={false}> to navigate
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app'
-// import { getAnalytics } from 'firebase/analytics'
-
 const H1 = styled('h1', {
   background: '$black',
   color: 'White',
@@ -19,13 +15,4 @@ export default function Home(props: any) {
     </>
   )
 }
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {}
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(app)
